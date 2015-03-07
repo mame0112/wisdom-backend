ce0fc8763670947f9e813d681ad5befc7fed6de3
mame0112 <mame0112@gmail.com>
2015-03-08 01:11:27 +0900

    Create template for new wisdom create page

--- war/js/wisdomCreateController.js
+++ war/js/wisdomCreateController.js
@@ -4,6 +4,11 @@ wisdomApp.controller('wisdomCreateController',
    'log',
     function($scope, $routeParams, log){
  	log.d("wisdomCreateController");
+
+	$scope.status = {
+		isopen1: false,
+		isopen1: false
+	};
  	// $scope.userId = $routeParams.userId;
  	// log.d("userId: " + $scope.userId);
 }]);
