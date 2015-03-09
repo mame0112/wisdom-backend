c11b9f662d72a535078d9e3a5b058aafe1caedf6
mame0112 <mame0112@gmail.com>
2015-03-10 01:24:38 +0900

    Add Input sub text/description field control

--- war/js/util/messageControlDirective.js
+++ war/js/util/messageControlDirective.js
@@ -5,9 +5,62 @@ wisdomApp.controller('messageOperationController',
  function($scope, log, modeService){
  	log.d("SigninController");
 
+ 	var DISPLAY = true;
+ 	var NOT_DISPLAY = false;
+
+	//TODO need to consider how we handle delete action
+ 	var textArray = [];
+
+ 	var descArray = [];
+
+ 	$scope.initialize = function()
+ 	{
+ 		$scope.panelCondition = NOT_DISPLAY;
+ 		$scope.titleState = DISPLAY;
+ 		$scope.descriptionState = NOT_DISPLAY;
+ 	};
+
+
  	$scope.operateMessage = function()
  	{
  		log.d("operateMessage");
  	};
+
+ 	$scope.onMouseOver = function()
+ 	{
+ 		log.d("onMouseOver");
+ 		$scope.panelCondition = DISPLAY;
+ 	};
+
+ 	$scope.onMouseLeave = function()
+ 	{
+ 		log.d("onMouseLeave");
+ 		$scope.panelCondition = NOT_DISPLAY;
+ 	};
+
+ 	$scope.showTitleField = function()
+ 	{
+ 		log.d("showTitleField");
+ 		$scope.titleState = DISPLAY;
+ 		$scope.descriptionState = NOT_DISPLAY;
+ 	};
+
+ 	$scope.showDescriptionField = function()
+ 	{
+ 		log.d("showDescriptionField");
+ 		$scope.titleState = NOT_DISPLAY;
+ 		$scope.descriptionState = DISPLAY;
+ 	};
+
+ 	$scope.saveSubTitleTexts = function(input)
+ 	{
+ 		log.d("saveSubTitleTexts: " + input);
+ 	};
+
+ 	$scope.saveSubDescriptionTexts = function(input)
+ 	{
+ 		log.d("saveSubDescriptionTexts: " + input);
+ 	};
+
 
 }]);
